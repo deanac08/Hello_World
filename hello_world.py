385bf8bbdc52cf1d3c2d61e39140921ba3db9181
deanac08 <deanac08@yahoo.com>
2022-07-06 09:49:11 -0700

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+print('Hello World')
+
+name = "Deana"
+print('Hello', name)
+print('Hello' + name)
+name = 42
+print( 'Hello', str(42))
+print( 'Hello'+ str(42))
+fave_food1 = "hot wings"
+fave_food2 = "ramen"
+print('I love to eat {} and {}'.format(fave_food1, fave_food2))
+print(f'I love to eat{fave_food1} and {fave_food2}')
